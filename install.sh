2554736fda4f0bfc70b2120e811ca3d448da604b
Juliann MERIT <juliann.merit@gmail.com>
2023-10-19 11:32:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bach
 
 # Créé l'environnement virtuel de python et installer les requirements
+set -e
 virtualenv -p python3 venv
-source .venv/bin/activate
+source "./venv/bin/activate"
 pip install -r requirements.txt
 pip install Werkzeug==2.3.7
+
+# Load la base de donnée
+flask loaddb src/data.yml
+
 deactivate
 
 # Crée le fichier .flaskenv
@@ -12,6 +17,5 @@ touch .flaskenv
 echo "FLASK_APP = src" > .flaskenv
 echo "FLASK_DEBUG = True" >> .flaskenv
 
-# Load la base de donnée
-flask loaddb src/data.yml
+
 
